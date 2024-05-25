7ff42765a7504739e54dd6642ef36d21c35cdb43
Wisp <1343347+wispborne@users.noreply.github.com>
2024-05-25 14:07:24 -0700

    Checks for nested folder when self updating.

--- lib/trios/self_updater/self_updater.dart
+++ lib/trios/self_updater/self_updater.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 import 'package:collection/collection.dart';
-import 'package:trios/utils/logging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart' as p;
 import 'package:pub_semver/pub_semver.dart';
 import 'package:trios/libarchive/libarchive.dart';
 import 'package:trios/trios/self_updater/script_generator.dart';
 import 'package:trios/utils/extensions.dart';
+import 'package:trios/utils/logging.dart';
 import 'package:trios/utils/network_util.dart';
 import 'package:trios/utils/util.dart';
 
@@ -18,7 +18,8 @@ class SelfUpdateInfo {
   final String url;
   final String releaseNote;
 
-  SelfUpdateInfo({required this.version, required this.url, required this.releaseNote});
+  SelfUpdateInfo(
+      {required this.version, required this.url, required this.releaseNote});
 
   factory SelfUpdateInfo.fromJson(Map<String, dynamic> json) {
     return SelfUpdateInfo(
@@ -36,9 +37,11 @@ class SelfUpdateInfo {
 
 class SelfUpdater {
   static const String githubBase = "https://api.github.com";
-  static const String githubLatestRelease = "$githubBase/repos/wispborne/trios/releases/latest";
+  static const String githubLatestRelease =
+      "$githubBase/repos/wispborne/trios/releases/latest";
 
-  static bool hasNewVersion(Release latestRelease, {String currentVersion = Constants.version}) {
+  static bool hasNewVersion(Release latestRelease,
+      {String currentVersion = Constants.version}) {
     try {
       final latestVersion = Version.parse(latestRelease.tagName);
       return Version.parse(currentVersion).compareTo(latestVersion) < 0;
@@ -50,27 +53,42 @@ class SelfUpdater {
   }
 
   static Future<void> update(Release release,
-      {bool exitSelfAfter = true, void Function(int, int)? downloadProgress}) async {
-    final updateWorkingDir = Directory.systemTemp.createTempSync('trios_update').absolute.normalize;
+      {bool exitSelfAfter = true,
+      void Function(int, int)? downloadProgress}) async {
+    final updateWorkingDir =
+        Directory.systemTemp.createTempSync('trios_update').absolute.normalize;
 
     // Download the release asset.
-    final downloadFile = await downloadRelease(release, updateWorkingDir, onProgress: (bytesReceived, contentLength) {
-      Fimber.v('Downloaded: ${bytesReceived.bytesAsReadableMB()} / ${contentLength.bytesAsReadableMB()}');
+    final downloadFile = await downloadRelease(release, updateWorkingDir,
+        onProgress: (bytesReceived, contentLength) {
+      Fimber.v(
+          'Downloaded: ${bytesReceived.bytesAsReadableMB()} / ${contentLength.bytesAsReadableMB()}');
       downloadProgress?.call(bytesReceived, contentLength);
     });
 
     Fimber.i('Downloaded update file: ${downloadFile.path}');
     final extractedDir = updateWorkingDir;
     // Extract the downloaded update archive.
-    final extractedFiles = await LibArchive().extractEntriesInArchive(downloadFile, extractedDir.path);
+    final extractedFiles = await LibArchive()
+        .extractEntriesInArchive(downloadFile, extractedDir.path);
 
     if (extractedFiles.isNotEmpty) {
-      downloadFile.deleteSync(); // Clean up the .zip file, we don't want to end up moving it in as part of the update.
-      Fimber.i('Extracted ${extractedFiles.length} files in the ${release.tagName} release to ${extractedDir.path}');
+      downloadFile
+          .deleteSync(); // Clean up the .zip file, we don't want to end up moving it in as part of the update.
+      Fimber.i(
+          'Extracted ${extractedFiles.length} files in the ${release.tagName} release to ${extractedDir.path}');
 
       // Generate the update script and write it to a file.
-      final scriptDest = kDebugMode ? Directory(p.join(Directory.current.path, "update-test")) : Directory.current;
-      final updateScriptFile = await ScriptGenerator.writeUpdateScriptToFileSimple(updateWorkingDir, scriptDest);
+      final scriptDest = kDebugMode
+          ? Directory(p.join(Directory.current.path, "update-trios"))
+          : Directory.current;
+      // If there's a subfolder, use the contents of the subfolder as the files to update.
+      final filesToUpdateFromPath = updateWorkingDir.listSync().length == 1
+          ? updateWorkingDir.listSync()[0].toDirectory()
+          : updateWorkingDir;
+      final updateScriptFile =
+          await ScriptGenerator.writeUpdateScriptToFileSimple(
+              filesToUpdateFromPath, scriptDest);
       Fimber.i("Wrote update script to: ${updateScriptFile.path}");
 
       Fimber.i('Running update script: ${updateScriptFile.path}');
@@ -78,7 +96,8 @@ class SelfUpdater {
       // Run the update script.
       // Do NOT wait for it. We want to exit immediately after starting the update script.
       if (Platform.isWindows) {
-        await Process.start("start", ["", updateScriptFile.absolute.normalize.path],
+        await Process.start(
+            "start", ["", updateScriptFile.absolute.normalize.path],
             runInShell: true, mode: ProcessStartMode.detached);
       } else {
         await Process.start('', [updateScriptFile.absolute.normalize.path, "&"],
@@ -112,16 +131,19 @@ class SelfUpdater {
     };
 
     final downloadLink = release.assets
-        .firstWhereOrNull((element) => element.name.toLowerCase().contains(assetNameForPlatform))
+        .firstWhereOrNull((element) =>
+            element.name.toLowerCase().contains(assetNameForPlatform))
         ?.browserDownloadUrl;
 
     if (downloadLink == null) {
-      throw Exception("No download link found for platform: $assetNameForPlatform");
+      throw Exception(
+          "No download link found for platform: $assetNameForPlatform");
     }
 
     Fimber.i("Download link: $downloadLink");
 
-    final downloadResult = await downloadFile(downloadLink, destDir, null, onProgress: onProgress);
+    final downloadResult =
+        await downloadFile(downloadLink, destDir, null, onProgress: onProgress);
     return downloadResult;
   }
 }

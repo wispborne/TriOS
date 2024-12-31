380a0d3165136499366a0bff4fa923bcff08b6e3
Wisp <1343347+wispborne@users.noreply.github.com>
2024-12-31 10:30:22 -0800

    1.0.0 RC

--- lib/trios/constants.dart
+++ lib/trios/constants.dart
@@ -4,7 +4,7 @@ import 'package:intl/intl.dart';
 import 'package:trios/utils/extensions.dart';
 
 class Constants {
-  static const version = "1.0.0-dev25";
+  static const version = "1.0.0-dev26";
 
   static const appName = "TriOS";
   static const appTitle = "$appName v$version";

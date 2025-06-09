172dcfd0ca0d3c06b1a97a1a3ae299f562c4745f
Wisp <1343347+wispborne@users.noreply.github.com>
2025-06-08 20:31:43 -0400

    1.2.1

--- lib/trios/constants.dart
+++ lib/trios/constants.dart
@@ -6,7 +6,7 @@ import 'package:trios/models/version.dart';
 import 'package:trios/utils/extensions.dart';
 
 class Constants {
-  static const version = "1.2.0";
+  static const version = "1.2.1";
   static Version currentVersion = Version.parse(version);
 
   static const appName = "TriOS";

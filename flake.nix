c83c3387719ff4645d1686704912e75b24da54d9
1337Dakota <72140289+1337Dakota@users.noreply.github.com>
2025-07-15 00:11:36 +0200

    Add flake, some issues still left, but it works

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+    description = "Starsector mod manager & toolkit";
+
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+    outputs = {
+        self,
+        nixpkgs,
+    }: let
+        forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+        systems = ["x86_64-linux" "aarch64-linux"];
+        version = "1.2.2";
+    in {
+        packages = forAllSystems (
+            system: let
+                pkgs = import nixpkgs {inherit system;};
+
+                trios-release = pkgs.fetchzip {
+                    url = "https://github.com/wispborne/TriOS/releases/download/${version}/TriOS-Linux.zip";
+                    hash = "sha256-12ktCyWJunvYbtTNixN6qDl9OeOj4b+MmoA7kK3JN10=";
+                };
+
+                runtimeLibs = with pkgs; [
+                    gtk3
+                    atk
+                    cairo
+                    curl
+                    libepoxy
+                    fontconfig
+                    gdk-pixbuf
+                    glib
+                    harfbuzz
+                    pango
+                    stdenv.cc.cc.lib
+                ];
+
+                trios = pkgs.stdenv.mkDerivation {
+                    pname = "trios";
+                    inherit version;
+                    src = trios-release;
+
+                    nativeBuildInputs = [pkgs.autoPatchelfHook pkgs.makeWrapper] ++ runtimeLibs;
+
+                    installPhase = ''
+                        mkdir -p $out/{bin,share/trios}
+                        cp -r * $out/share/trios
+
+                        chmod +x $out/share/trios/data/flutter_assets/assets/linux/7zip/{arm64,x64}/7zzs
+
+                        makeWrapper $out/share/trios/TriOS $out/bin/TriOS \
+                            --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath runtimeLibs}:$out/share/trios/lib \
+                            --prefix PATH : ${pkgs.lib.makeBinPath [pkgs.zenity]}
+                    '';
+
+                    dontStrip = true;
+                };
+            in {default = trios;}
+        );
+
+        apps = forAllSystems (system: {
+            default = {
+                type = "app";
+                program = "${self.packages.${system}.default}/bin/TriOS";
+            };
+        });
+
+        defaultPackage = forAllSystems (system: self.packages.${system}.default);
+        defaultApp = forAllSystems (system: self.apps.${system}.default);
+    };
+}

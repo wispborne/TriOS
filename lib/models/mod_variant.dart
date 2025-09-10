6054ca6796b29c73151deac56fd67ef5f36dbde7
Wisp <1343347+wispborne@users.noreply.github.com>
2025-09-09 22:36:30 -0400

    Fixed very dumb smolId caching bug.

--- lib/models/mod_variant.dart
+++ lib/models/mod_variant.dart
@@ -154,13 +154,16 @@ final smolIdAllowedChars = RegExp(r'[^0-9a-zA-Z\\.\-_]');
 final _smolIdCache = <String, String>{};
 
 String createSmolId(String id, Version? version) {
-  if (_smolIdCache.containsKey(id)) {
-    return _smolIdCache[id]!;
+  final versionString = version.toString();
+  final cacheKey = '$id-$versionString';
+
+  if (_smolIdCache.containsKey(cacheKey)) {
+    return _smolIdCache[cacheKey]!;
   }
 
   final result =
-      '${id.replaceAll(smolIdAllowedChars, '').take(6)}-${version.toString().replaceAll(smolIdAllowedChars, '').take(9)}-${(id.hashCode + version.hashCode).abs()}';
-  _smolIdCache[id] = result;
+      '${id.replaceAll(smolIdAllowedChars, '').take(6)}-${versionString.replaceAll(smolIdAllowedChars, '').take(9)}-${(id.hashCode + version.hashCode).abs()}';
+  _smolIdCache[cacheKey] = result;
 
   if (_smolIdCache.length > 1000) {
     _smolIdCache.clear();
